2f7bccc810cdf7330c7ab73ffbd7ccc168068b71
Goran Bunjevac <bunjevacgoran5@gmail.com>
2018-10-23 10:10:48 +0200

    Adding Responsivenes

--- my-burger-app/src/components/Layout/Layout.js
+++ my-burger-app/src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
 import "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
+    <SideDrawer />
     <main className="Content">{props.children}</main>
   </Aux>
 );
