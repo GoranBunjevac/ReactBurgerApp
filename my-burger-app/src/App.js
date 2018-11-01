65c034a3915029da7d6cf9d50e5215bf759cb414
Goran Bunjevac <bunjevacgoran5@gmail.com>
2018-11-01 17:17:31 +0100

    Checkout added

--- my-burger-app/src/App.js
+++ my-burger-app/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
 import Burgerbuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <Burgerbuilder />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/" exact component={Burgerbuilder} />
         </Layout>
       </div>
     );
--- my-burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,29 +93,20 @@ class Brugerbuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Gogo Bunjevac",
-        adress: {
-          street: "Zvijezda",
-          zipCode: "71330",
-          country: "BiH"
-        },
-        email: "gogo@gogo.com"
-      },
-      deliveryMethod: "fastest"
-    };
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          "=" +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+    queryParams.push("price=" + this.state.totalPrice);
+    const queryString = queryParams.join("&");
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString
+    });
   };
 
   render() {
